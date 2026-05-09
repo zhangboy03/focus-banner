c0b9fc4c59290885cbc73ae8a565fdee8568294f
Boyu Zhang <boyu94828@gmail.com>
2026-05-09 15:06:19 +0800

    feat: add Toggle Work Mode menu command

--- focus-banner.user.js
+++ focus-banner.user.js
@@ -15,5 +15,18 @@
 (function () {
   'use strict';
 
-  console.log('[focus-banner] loaded on', location.hostname);
+  function toggleWorkMode() {
+    const next = !GM_getValue('work-mode', false);
+    GM_setValue('work-mode', next);
+    GM_notification({
+      title: 'Focus Banner',
+      text: `Work Mode: ${next ? 'ON 🔥' : 'OFF 💤'}`,
+      timeout: 2000
+    });
+  }
+
+  GM_registerMenuCommand('🔥 切换 Work Mode', toggleWorkMode);
+
+  console.log('[focus-banner] loaded on', location.hostname,
+              '| work-mode:', GM_getValue('work-mode', false));
 })();
